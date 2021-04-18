ebd74588ea5ae373877c714657799c9b953f3743
Dipti2021 <77368913+Dipti2021@users.noreply.github.com>
2021-04-17 22:15:27 -0400

    Updated the function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,46 @@
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
 
+  } else if (license === 'GNU') {
+    return '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)'
 
+  } else if (license === 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
 
+  } else {
+    return ''
+  }
+
+}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT)'
+
+  } else if (license === 'GNU') {
+    return '[https://img.shields.io/badge/License-GPL%20v2-blue.svg](https://img.shields.io/badge/License-GPL%20v2-blue.svg)'
+
+  } else if (license === 'Apache') {
+    return '[https://opensource.org/licenses/Apache-2.0](https://opensource.org/licenses/Apache-2.0)'
+
+  } else {
+    return ''
+  }
+}
+function renderLicenseSection(license) {
+  if (license !== "None of the above") {
+    return `Look for the project under ${license}`
+  } else {
+    return "Please choose a licence as you don't have one."
+  }
+}
 
 
 function generateMarkdown(data) {
   return `# ${data.title}
   ## License:
-  [![license](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://shields.io/)
+  ${renderLicenseBadge(data.license)}
+
   ## Table of Contents 
   - [Description](#description)
   - [Installation](#installation)
@@ -17,16 +51,23 @@ function generateMarkdown(data) {
 
   ## Description:
   ${data.description}
+
   ## Installation:
   ${data.installation}
+
   ## Usage:
   ${data.usage}
+
   ## License: 
-  ${data.license}
+ ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+
   ## Contribution: 
   ${data.contribution}
+
   ## Testing: 
   ${data.test}
+
   ## Contact information:
   - Github: [${data.github}](https://github.com/${data.github})
   - Email: [${data.email}](mailto:user@example.com) `;
