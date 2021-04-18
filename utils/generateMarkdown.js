7b9701e209006790ebe989988ef6dd65f4d6f342
Dipti2021 <77368913+Dipti2021@users.noreply.github.com>
2021-04-18 09:29:49 -0400

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,13 @@
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    return `[![license:MIT](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://shields.io/)`
+    
 
   } else if (license === 'GNU') {
-    return '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://img.shields.io/badge/License-GPL%20v2-blue.svg)'
+    return `[![license:GNU](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://shields.io/)`
 
   } else if (license === 'Apache') {
-    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    return `[![license:](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://shields.io/)`
 
   } else {
     return ''
@@ -40,7 +41,7 @@ function generateMarkdown(data) {
   return `# ${data.title}
   ## License:
   ${renderLicenseBadge(data.license)}
-
+  [![license](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://shields.io/)
   ## Table of Contents 
   - [Description](#description)
   - [Installation](#installation)
@@ -48,31 +49,22 @@ function generateMarkdown(data) {
   - [Contribution](#contribution)
   - [Testing](#testing)
   - [Additional Info](#additional-info)
-
   ## Description:
   ${data.description}
-
   ## Installation:
   ${data.installation}
-
   ## Usage:
   ${data.usage}
-
   ## License: 
  ${renderLicenseSection(data.license)}
   ${renderLicenseLink(data.license)}
-
   ## Contribution: 
   ${data.contribution}
-
   ## Testing: 
   ${data.test}
-
   ## Contact information:
   - Github: [${data.github}](https://github.com/${data.github})
   - Email: [${data.email}](mailto:user@example.com) `;
 }
 
 module.exports = generateMarkdown;
-
-
